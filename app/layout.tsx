f0c63ff6c3fa74f7e964287bdf7e20b53dc2dfdf
Marie Ariana Alexi B. Guial <158830223+ArianaGuial@users.noreply.github.com>
2024-09-09 15:08:31 +0800

    Next.js course

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -8,6 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
